a9beea20bd3393ee0287322885f7d7b61d546202
Lucas D'Alesio <dalesil@rose-hulman.edu>
2018-11-26 13:27:42 -0500

    M6 Exercise Done

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Lucas D'Alesio.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,29 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+red_turtle = rg.SimpleTurtle('turtle')
+rg.Pen('red', 30)
+red_turtle.speed = 5
+size = 100
+for k in range(10):
+    red_turtle.draw_square(size)
+    red_turtle.pen_up()
+    red_turtle.right(100)
+    red_turtle.backward(100)
+    red_turtle.pen_down()
+    size = size - 10
+
+green_turtle = rg.SimpleTurtle('arrow')
+rg.Pen('green', 20)
+green_turtle.speed = 8
+size = 200
+for k in range(20):
+    green_turtle.draw_circle(size)
+    green_turtle.pen_up()
+    green_turtle.forward(100)
+    green_turtle.right(50)
+    green_turtle.backward(100)
+    green_turtle.pen_down()
+    size = size - 20
